db69980088089157b5a915357b87362cfa9c2da5
MJGR <mjgriley@gmail.com>
2021-09-21 22:20:28 -0400

    timer working!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,24 @@
-var timer = document.getElementById("timer");
-var 
+var timer = {
+    html: document.getElementById("timer"),
+    time: 0,
+}  
+
+console.log(timer.html);
+
+
+setInterval(function () {
+    timer.time++;
+    timer.html.textContent = timer.time;
+    console.log(timer.html);
+    console.log(timer.time);
+
+}, 1000)
+
+var sBtn = document.getElementById("start")
+//sBtn.addEventListener('click', function () {
+
+//})
+
+//function init() {
+    //timer = 0
+//}
